f946d57eca3ac2aabb2bd565234c6d00c8058dd9
Junpeng <623968625@qq.com>
2019-07-07 16:57:16 +0800

    初始化Travel项目

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    helloworld
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
-<style></style>
+<style scoped></style>
