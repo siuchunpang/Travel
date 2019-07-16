497acd1d4cd4f6aa9abbe696bb65236d762afdb0
Junpeng <623968625@qq.com>
2019-07-17 00:24:03 +0800

    Vuex实现数据共享

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -5,14 +5,19 @@
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
           <div class="button-wrapper">
-            <div class="button">北京</div>
+            <div class="button">{{this.$store.state.city}}</div>
           </div>
         </div>
       </div>
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper" v-for="item of hotCities" :key="item.id">
+          <div
+            class="button-wrapper"
+            v-for="item of hotCities"
+            :key="item.id"
+            @click="handleCityClick(item.name)"
+          >
             <div class="button">{{item.name}}</div>
           </div>
         </div>
@@ -24,7 +29,12 @@
         :ref="key"
       >
         <div class="title border-topbottom">{{key}}</div>
-        <div class="item-list" v-for="innerItem of item" :key="innerItem.id">
+        <div
+          class="item-list"
+          v-for="innerItem of item"
+          :key="innerItem.id"
+          @click="handleCityClick(innerItem.name)"
+        >
           <div class="item border-bottom">{{innerItem.name}}</div>
         </div>
       </div>
@@ -41,8 +51,11 @@ export default {
     hotCities: Array,
     letter: String
   },
-  mounted() {
-    this.scroll = new Bscroll(this.$refs.wrapper)
+  methods: {
+    handleCityClick(city) {
+      this.$store.commit('changeCity', city)
+      this.$router.push('/')
+    }
   },
   watch: {
     letter() {
@@ -51,6 +64,9 @@ export default {
         this.scroll.scrollToElement(element)    
       }
     }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.wrapper)
   }
 }
 </script>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -5,7 +5,14 @@
     </div>
     <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >
+        {{item.name}}
+        </li>
         <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
       </ul>
     </div>
@@ -31,8 +38,11 @@ export default {
       return !this.list.length
     }
   },
-  mounted() {
-    this.scroll = new Bscroll(this.$refs.search)
+  methods: {
+    handleCityClick(city) {
+      this.$store.commit('changeCity', city)
+      this.$router.push('/')
+    }
   },
   watch: {
     keyword() {
@@ -53,9 +63,12 @@ export default {
           });
         }
         this.list = result
-      }, 100);
+      }, 16);
     }
-  }
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
 }
 </script>
 
