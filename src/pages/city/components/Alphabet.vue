81103654abf5544ea904d7a362419afa92cd1970
Junpeng <623968625@qq.com>
2019-07-16 21:50:30 +0800

    搜索逻辑实现

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -57,7 +57,7 @@ export default {
             this.$emit('change', this.letters[index])
           }
         }
-      })
+      }, 16)
     },
     handleTouchEnd() {
       this.touchStatus = false
